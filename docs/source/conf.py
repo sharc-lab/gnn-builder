a8894b9034c28d42359d210223d0aec474e083a3
Stefan Abi-Karam <stefanabikaram@gmail.com>
2023-08-30 16:59:26 -0400

    setup docs

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'gnn-builder'
+copyright = "2023, Sharc Lab"
+author = "Sharc Lab"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+
+html_css_files = [
+    'custom.css',
+]
---
+++ docs/source/overview.rst
@@ -0,0 +1,12 @@
+==================
+Framework Overview
+==================
+
+GNNBuilder has several components that work together in a larger, framework-defined workflow. Both the components and the workflow are exposed to the user via the GNNBuilder library. This enables users to create their own workflows or integrate GNNBuilder into their current experiments and design flows for their downstream applications of interest.
+
+
+
+
+
+
+
